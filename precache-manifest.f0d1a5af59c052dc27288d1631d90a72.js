a1d8089983a3ed29a4d26129fb28d8dff8f86388
RohanPatil1 <prorohan8@gmail.com>
2020-10-23 12:43:12 +0530

    Updates

---
+++ precache-manifest.f0d1a5af59c052dc27288d1631d90a72.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716a2b5924ec36e32e7484b11c60d25b",
+    "url": "/weatheringyou/index.html"
+  },
+  {
+    "revision": "ba74f7ff825f53b48e1e",
+    "url": "/weatheringyou/static/css/2.d0fa1b42.chunk.css"
+  },
+  {
+    "revision": "e44c20b275631af1e459",
+    "url": "/weatheringyou/static/css/main.32a58668.chunk.css"
+  },
+  {
+    "revision": "ba74f7ff825f53b48e1e",
+    "url": "/weatheringyou/static/js/2.5e761456.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/weatheringyou/static/js/2.5e761456.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e44c20b275631af1e459",
+    "url": "/weatheringyou/static/js/main.a5901653.chunk.js"
+  },
+  {
+    "revision": "0ae0cfe64a4eccf8f33a",
+    "url": "/weatheringyou/static/js/runtime-main.176c6b4a.js"
+  },
+  {
+    "revision": "42906aabad0e3eb7aec84198f38ec084",
+    "url": "/weatheringyou/static/media/finalclip.42906aab.mp4"
+  },
+  {
+    "revision": "2ca3df0981116e195ebc62dd1dcc8b9b",
+    "url": "/weatheringyou/static/media/weathericons-regular-webfont.2ca3df09.ttf"
+  },
+  {
+    "revision": "b2233bc1525939550405c87cffede242",
+    "url": "/weatheringyou/static/media/weathericons-regular-webfont.b2233bc1.svg"
+  },
+  {
+    "revision": "e7ef2b448d27cf5312a73ceb3e7841c4",
+    "url": "/weatheringyou/static/media/weathericons-regular-webfont.e7ef2b44.eot"
+  },
+  {
+    "revision": "f5b0d0d9cb02e551003132e079435312",
+    "url": "/weatheringyou/static/media/weathericons-regular-webfont.f5b0d0d9.woff"
+  }
+]);
